fdb38e8a9722a6a127e8785a1ac4da82b359c277
Sravan T P <sravantp03@gmail.com>
2023-02-10 17:33:18 +0530

    evaluated gas usage using hardhat-gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,12 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -17,12 +19,19 @@ module.exports = {
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
-            // accounts: [] This will prick by hardhat automatically
+            // accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"] This will prick by hardhat automatically
             chainId: 31337,
         },
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     solidity: "0.8.17",
 }
